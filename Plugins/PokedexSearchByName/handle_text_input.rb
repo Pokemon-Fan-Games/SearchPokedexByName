3be6587911c1166aeae3cd2000091a499c64a091
Diego Pertierra <dpertierra@hotmail.com>
2024-08-02 17:40:43 +0200

    Code cleanup and optimizations

---
+++ Plugins/PokedexSearchByName/handle_text_input.rb
@@ -0,0 +1,177 @@
+# This class represents a window for text entry with a keyboard per key.
+#
+# It extends the `Window_TextEntry` class and adds functionality for handling keyboard input.
+#
+# @author DPertierra
+# @version 1.0.0
+#
+class WindowTextEntryKeyboardPerKey < Window_TextEntry
+  # ...
+
+  ##
+  # Initializes the window with the given text, position, width, height, heading, and options.
+  #
+  # @param text [String] The initial text to display in the window.
+  # @param x [Integer] The x-coordinate of the window's position.
+  # @param y [Integer] The y-coordinate of the window's position.
+  # @param width [Integer] The width of the window.
+  # @param height [Integer] The height of the window.
+  # @param heading [String, nil] The heading to display above the text entry area.
+  # @param usedarkercolor [Boolean] Whether to use a darker color for the window.
+  # @param on_input [Proc, nil] The callback to invoke when a key is pressed.
+  #
+  def initialize(text, x, y, width, height, heading = nil, usedarkercolor = false, on_input = nil)
+    super(text, x, y, width, height, heading, usedarkercolor)
+    @on_input = on_input
+  end
+
+  ##
+  # Deletes the last character in the text entry area.
+  #
+  # @return [void]
+  #
+  def delete
+    return false unless @helper.delete
+
+    @cursor_timer_start = System.uptime
+    @cursor_shown = true
+    @on_input&.call(@helper.text)
+    refresh
+
+    true
+  end
+
+  def insert(ch)
+    return false unless @helper.insert(ch)
+
+    @cursor_timer_start = System.uptime
+    @cursor_shown = true
+    refresh
+    @on_input&.call(@helper.text, ch)
+
+    true
+  end
+
+  def move_left
+    return unless @helper.cursor.positive?
+
+    @helper.cursor -= 1
+    @cursor_timer_start = System.uptime
+    @cursor_shown = true
+    refresh
+  end
+
+  def move_right
+    return unless @helper.cursor < text.scan(/./m).length
+
+    @helper.cursor += 1
+    @cursor_timer_start = System.uptime
+    @cursor_shown = true
+    refresh
+  end
+
+  def handle_input
+    # Moving cursor
+    if Input.triggerex?(:LEFT) || Input.repeatex?(:LEFT)
+      move_left
+    elsif Input.triggerex?(:RIGHT) || Input.repeatex?(:RIGHT)
+      move_right
+    elsif Input.triggerex?(:BACKSPACE) || Input.repeatex?(:BACKSPACE)
+      delete if @helper.cursor.positive?
+    elsif Input.triggerex?(:RETURN) || Input.triggerex?(:ESCAPE)
+      # return
+    else
+      Input.gets.each_char { |c| insert(c) }
+    end
+  end
+
+  def update
+    cursor_to_show = ((System.uptime - @cursor_timer_start) / 0.35).to_i.even?
+    if cursor_to_show != @cursor_shown
+      @cursor_shown = cursor_to_show
+      refresh
+    end
+
+    return unless active
+
+    handle_input
+  end
+end
+
+def pb_free_text_with_on_input(msg_window, current_text, password_box, max_length, width = 240, on_input = nil)
+  window = Window_TextEntry_Keyboard_Per_Key.new(current_text, 0, 0, width, 64, nil, false, on_input)
+  configure_window(window, msg_window, password_box, max_length)
+
+  Input.text_input = true
+  loop do
+    break if handle_input(window, msg_window, current_text)
+  end
+  Input.text_input = false
+  window.dispose
+  Input.update
+  window.text
+end
+
+def handle_input(window, msg_window, current_text = '')
+  Graphics.update
+  Input.update
+  if Input.triggerex?(:ESCAPE)
+    window.text = current_text
+    return
+  elsif Input.triggerex?(:RETURN)
+    return
+  end
+
+  window.update
+  msg_window&.update
+  yield if block_given?
+end
+
+def configure_window(window, msg_window, password_box, max_length)
+  window.maxlength = max_length
+  window.visible = true
+  window.z = 99_999
+  pb_position_near_msg_window(window, msg_window, :right)
+  window.text = current_text
+  window.password_char = '*' if password_box
+end
+
+# def pbFreeTextWithOnInput(msgwindow, currenttext, passwordbox, maxlength, width = 240, on_input = nil)
+#   window = Window_TextEntry_Keyboard_Per_Key.new(currenttext, 0, 0, width, 64, heading = nil, usedarkercolor = false, on_input)
+#   ret = ''
+#   window.maxlength = maxlength
+#   window.visible = true
+#   window.z = 99_999
+#   pbPositionNearMsgWindow(window, msgwindow, :right)
+#   window.text = currenttext
+#   window.passwordChar = '*' if passwordbox
+#   Input.text_input = true
+#   loop do
+#     Graphics.update
+#     Input.update
+#     if Input.triggerex?(:ESCAPE)
+#       ret = currenttext
+#       break
+#     elsif Input.triggerex?(:RETURN)
+#       ret = window.text
+#       break
+#     end
+#     window.update
+#     msgwindow&.update
+#     yield if block_given?
+#   end
+#   Input.text_input = false
+#   window.dispose
+#   Input.update
+#   ret
+# end
+
+def pb_message_free_text_with_on_input(message, currenttext, passwordbox, maxlength, width = 240, on_input = nil, &block)
+  msgwindow = pbCreateMessageWindow
+  retval = pbMessageDisplay(msgwindow, message, true,
+                            proc { |msgwndw|
+                              next pb_free_text_with_on_input(msgwndw, currenttext, passwordbox, maxlength, width, on_input, &block)
+                            }, &block)
+  pbDisposeMessageWindow(msgwindow)
+  retval
+end
---
+++ Plugins/PokedexSearchByName/pokedex_search_by_name.rb
@@ -0,0 +1,95 @@
+class PokemonPokedex_Scene
+  attr_reader :sprites
+end
+
+# Searches for a Pokémon in the dexlist based on the given text.
+#
+# @param text [String] The name of the Pokémon to search for.
+# @param _char [String] (optional) The character to search for. Defaults to an empty string.
+# @return [Boolean, nil] Returns `true` if the Pokémon is found and refreshes the dexlist. Returns `false` if no Pokémon is found.
+class PokedexSearcher
+  def initialize(dexlist, dex_instance)
+    @dexlist = dexlist
+    @dex_instance = dex_instance
+    open_search_box
+  end
+
+  def open_search_box
+    on_input = ->(text, char = '') { search_by_name(text, char) }
+    term = pbMessageFreeTextWithOnInput(_INTL('¿Qué Pokémon desea buscar?'), '', false, 32, width = 240, on_input = on_input)
+
+    return false if ['', nil].include?(term)
+
+    search_by_name(term)
+  end
+
+  def search_by_name(text, _char = '')
+    current_index = @dex_instance.sprites['pokedex'].index
+    index = search(text, current_index, @dexlist.length)
+    return @dex_instance.pbRefreshDexList(index) if index
+
+    if current_index.positive?
+      index = search(text, 0, current_index)
+      return @dex_instance.pbRefreshDexList(index) if index
+    end
+    false
+  end
+
+  def search(text, start_index, end_index)
+    @dexlist[start_index...end_index].each do |item|
+      next unless valid_item?(item)
+      return item[:number] - 1 if matches_name?(item, text)
+    end
+    false
+  end
+
+  def valid_item?(item)
+    ($player.seen?(item[:species]) && !item[:shift]) || $player.seen?(item[:species])
+  end
+
+  def matches_name?(item, text)
+    item[:name].downcase.include?(text.downcase)
+  end
+end
+
+##
+# This class represents a scene for the Pokemon Pokedex.
+# It provides methods for opening a search box and searching for Pokemon by name.
+class PokemonPokedex_Scene
+  def open_search_box
+    PokedexSearcher.new(@dexlist, self)
+  end
+
+  def pbPokedex
+    pbActivateWindow(@sprites, 'pokedex') do
+      loop do
+        Graphics.update
+        Input.update
+        oldindex = @sprites['pokedex'].index
+        pbUpdate
+        if oldindex != @sprites['pokedex'].index
+          $PokemonGlobal.pokedexIndex[pbGetSavePositionIndex] = @sprites['pokedex'].index unless @searchResults
+          pbRefresh
+        end
+        if Input.trigger?(Input::ACTION)
+          pbSEPlay('GUI pokedex open')
+          @sprites['pokedex'].active = false
+          pbDexSearch
+          @sprites['pokedex'].active = true
+        elsif Input.trigger?(Input::BACK)
+          pbPlayCloseMenuSE
+          break unless @searchResults
+
+          pbCloseSearch
+        elsif Input.trigger?(Input::USE)
+          if $player.seen?(@sprites['pokedex'].species)
+            pbSEPlay('GUI pokedex open')
+            pbDexEntry(@sprites['pokedex'].index)
+          end
+        elsif Input.trigger?(Input::SPECIAL)
+          open_search_box
+        end
+      end
+    end
+  end
+end
